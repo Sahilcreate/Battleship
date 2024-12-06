281ce4350152756a153af7c149b2e34e04f5bf4b
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-12-06 21:04:24 +0530

    feat: add player (player & computer) logic

---
+++ src/player.js
@@ -0,0 +1,26 @@
+import Gameboard from "./gameboard";
+
+export default class Player {
+  constructor(name) {
+    this.name = name;
+    this.playerBoard = new Gameboard();
+    this.previousAttack = new Set();
+  }
+
+  getRandomCoordinate() {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    } while (this.previousAttack.has(`${x},${y}`));
+    this.previousAttack.add(`${x},${y}`);
+    return [x, y];
+  }
+
+  attackEnemy(enemy, coordinate = null) {
+    if (this.name === "Computer") {
+      coordinate = this.getRandomCoordinate();
+    }
+    return enemy.playerBoard.receiveAttack(coordinate);
+  }
+}
