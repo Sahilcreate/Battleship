7787a387caf307240a7bc99cdb1540fdc3174d65
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-12-07 21:03:35 +0530

    feat: add elementary function for rendering board

--- src/view.js
+++ src/view.js
@@ -2,6 +2,55 @@ const playerContainer = document.querySelector(".player-container");
 const computerContainer = document.querySelector(".computer-container");
 const info = document.querySelector(".info");
 
+export default function renderTurnBoards(player, computer) {
+  const playerArray = player.playerBoard.board;
+  const computerArray = computer.playerBoard.board;
+
+  playerArray.forEach((row) =>
+    row.forEach((cell) => {
+      const cellDiv = createCell(cell, "player");
+      playerContainer.append(cellDiv);
+    })
+  );
+  computerArray.forEach((row) =>
+    row.forEach((cell) => {
+      const cellDiv = createCell(cell, "computer");
+      computerContainer.append(cellDiv);
+
+      //   if (cell.hitStatus === false) {
+      //     addComputerCellEvent(
+      //       cellDiv,
+      //       cell,
+      //       computer.receiveAttack(cell.coordinate)
+      //     );
+      //   } else if (cell.hitStatus === true) {
+      //   }
+    })
+  );
+}
+
+function createCell(cell, user) {
+  const cellDiv = document.createElement("div");
+  cellDiv.setAttribute("data-coord", cell.coordinate);
+
+  if (user === "computer") {
+    cellDiv.className = "computer-cell";
+    return cellDiv;
+  } else if (user === "player") {
+    cellDiv.className = "player-cell";
+    return cellDiv;
+  }
+}
+
+function addComputerCellEvent(cellDiv, callback) {
+  cellDiv.addEventListener("click", callback());
+}
+
+//☠︎︎
+//🔥
+//⭐
+//〰️
+
 //create two boards for player and computer
 // Each board have 100 cell as div and each div
 // have data-coord as [x,y].
