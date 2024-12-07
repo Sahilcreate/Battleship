c1a17e2df50b56094b1a51fdb75144891cf476aa
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-12-07 20:58:07 +0530

    feat: elementary function for starting game

--- src/game.js
+++ src/game.js
@@ -1,1 +1,12 @@
 import Player from "./player";
+import renderTurnBoards from "./view";
+
+export default function gameStart() {
+  const player = new Player("player");
+  const computer = new Player("computer");
+
+  player.playerBoard.placeShipsRandomly();
+  computer.playerBoard.placeShipsRandomly();
+
+  renderTurnBoards(player, computer);
+}
