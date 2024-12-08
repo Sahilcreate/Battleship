e0e4409b5073a8d8c30e37a6072f05bc15df3ea4
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-12-08 22:18:29 +0530

    delete: functions are added in view.js

--- src/game.js
+++ src/game.js
@@ -1,12 +1,23 @@
-import Player from "./player";
+// import Player from "./player";
 import renderTurnBoards from "./view";
 
-export default function gameStart() {
-  const player = new Player("player");
-  const computer = new Player("computer");
+// export default function gameStart() {
+//   const player = new Player("player");
+//   const computer = new Player("computer");
 
-  player.playerBoard.placeShipsRandomly();
-  computer.playerBoard.placeShipsRandomly();
+//   player.playerBoard.placeShipsRandomly();
+//   computer.playerBoard.placeShipsRandomly();
 
-  renderTurnBoards(player, computer);
-}
+//   renderTurnBoards(player, computer);
+// }
+
+//how to change turns
+
+//let a variable, and store current player
+//in this variable.
+//if variable is computer,
+//launch attack on enemy
+//change variable to player
+//if variable is player
+//launch attack on click
+//change variable to computer
